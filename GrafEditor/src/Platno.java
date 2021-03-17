95dbc1c7d562dac7a418ffd324994259dcd3708a
Gaja Jamnik <gaja.jamnik@gmail.com>
2021-03-17 09:47:29 +0100

    posodbljeno platno

--- GrafEditor/src/Platno.java
+++ GrafEditor/src/Platno.java
@@ -3,15 +3,27 @@ import java.awt.Color;
 import java.awt.Dimension;
 import java.awt.Graphics;
 import java.awt.Graphics2D;
+import java.awt.event.KeyEvent;
+import java.awt.event.KeyListener;
+import java.awt.event.MouseEvent;
+import java.awt.event.MouseListener;
+import java.awt.event.MouseMotionListener;
+import java.util.HashSet;
+import java.util.Set;
 
 import javax.swing.JPanel;
 
 @SuppressWarnings("serial")
-public class Platno extends JPanel {
+public class Platno extends JPanel implements MouseListener, MouseMotionListener, KeyListener {
 	
 	protected Graf graf;
+	protected Tocka aktivnaTocka;
+	protected Set<Tocka> izbraneTocke;
+	
 	protected Color barvaPovezave;
 	protected Color barvaTocke;
+	protected Color barvaAktivneTocke;
+	protected Color barvaIzbraneTocke;
 	protected Color barvaRoba;
 	protected float debelinaPovezave;
 	protected float debelinaRoba;
@@ -21,17 +33,28 @@ public class Platno extends JPanel {
 		super(); //poklices konstruktor nadrazreda
 		setPreferredSize(new Dimension(sirina, visina));
 		graf = null;
+		aktivnaTocka = null;
+		izbraneTocke = new HashSet<Tocka>();
+		
 		this.barvaPovezave = Color.GRAY;
-		this.barvaTocke = Color.MAGENTA;
+		this.barvaTocke = Color.BLACK;
+		this.barvaAktivneTocke = Color.MAGENTA;
+		this.barvaIzbraneTocke = Color.YELLOW;
 		this.barvaRoba = Color.CYAN;
 		this.debelinaPovezave = 3;
 		this.debelinaRoba = 4;
 		this.polmer = 20;
 		
+		addMouseListener(this); //sele s tem poves, da se nekaj zgodi ko nardis nekaj z misko
+		addMouseMotionListener(this);
+		addKeyListener(this);
+		setFocusable(true); //nastavis da je platno sposobno prejet focus in reagira na tipkovnico
 	}
 	
 	public void nastaviGraf(Graf graf) {
 		this.graf = graf;
+		aktivnaTocka = null;
+		izbraneTocke.clear();
 		repaint(); // z to metodo zahtevas da se slika obnovi
 	}
 	
@@ -49,6 +72,7 @@ public class Platno extends JPanel {
 		g2D.setColor(barvaPovezave);
 		g2D.setStroke(new BasicStroke(this.debelinaPovezave));
 		
+		//risanje povezav
 		for (Tocka tocka : graf.tocke.values()) {
 			for (Tocka sosed : tocka.sosedi) {
 				if (tocka.ime.compareTo(sosed.ime) < 0) { //pogoj da vsako crto samo enx narisemo
@@ -59,13 +83,109 @@ public class Platno extends JPanel {
 		
 		// za risanje crt navaden g za risanje ovalov rabimo Graphics torej 2D
 		
+		//risanje tock
 		for (Tocka tocka : graf.tocke.values()) {
 			g2D.setColor(barvaRoba);
 			g2D.fillOval(round(tocka.x - polmer - debelinaRoba), round(tocka.y - polmer - debelinaRoba), round(polmer + debelinaRoba) * 2, round(polmer + debelinaRoba) * 2);
-			g2D.setColor(barvaTocke);
+			if (tocka == aktivnaTocka) g.setColor(barvaAktivneTocke);
+			else if (izbraneTocke.contains(tocka)) g.setColor(barvaIzbraneTocke);
+			else g.setColor(barvaTocke);
 			g2D.fillOval(round(tocka.x - polmer), round(tocka.y - polmer), polmer * 2, polmer * 2); //notranjost
 		}
 	}
+	
+	private int stariX, stariY;
+	private int klikX, klikY;
+	
+	@Override
+	public void mousePressed(MouseEvent e) {
+		int x = e.getX();
+		int y = e.getY();
+		klikX = x;
+		klikY = y;
+		stariX = x;
+		stariY = y;
+		Tocka izbranaTocka = null;
+		for (Tocka u : graf.tocke.values()) {
+			if ((u.x - x)*(u.x - x) + (u.y - y)*(u.y - y) < polmer*polmer) aktivnaTocka = u;
+		}
+		repaint();
+		
+	}
+	
+	@Override
+	public void mouseDragged(MouseEvent e) {
+		int x = e.getX();
+		int y = e.getY();
+		if (aktivnaTocka != null) {
+			int razlikaX = x - stariX;
+			int razlikaY = y - stariY;
+			aktivnaTocka.x += razlikaX;
+			aktivnaTocka.y += razlikaY;
+		}
+		else {
+			for (Tocka u : izbraneTocke) {
+				int razlikaX = x - stariX;
+				int razlikaY = y - stariY;
+				u.x += razlikaX;
+				u.y += razlikaY;
+			}
+		}
+		stariX = x;
+		stariY = y;
+		repaint();
+		
+	}
+	
+	@Override
+	public void mouseReleased(MouseEvent e) {
+		int x = e.getX();
+		int y = e.getY();
+		//se ne premakne
+		if (klikX == x && klikY == y) {
+			if (aktivnaTocka != null) {
+				if (izbraneTocke.contains(aktivnaTocka)) izbraneTocke.remove(aktivnaTocka);
+				else izbraneTocke.add(aktivnaTocka);
+			}
+			else {
+				Tocka v = graf.dodajTocko();
+				v.x = x;
+				v.y = y;
+				for (Tocka u : izbraneTocke) {
+					graf.dodajPovezavo(v, u);
+				}
+			}
+		
+		}
+		aktivnaTocka = null;
+		repaint();
+		
+		
+	}
+	
+	@Override
+	public void keyTyped(KeyEvent e) {
+		System.out.println("typed");
+		
+	}
+	
+	@Override
+	public void mouseClicked(MouseEvent e) {}
+
+	@Override
+	public void mouseEntered(MouseEvent e) {}
+
+	@Override
+	public void mouseExited(MouseEvent e) {}
+
+	@Override
+	public void mouseMoved(MouseEvent e) {}
+
+	@Override
+	public void keyPressed(KeyEvent e) {}
+
+	@Override
+	public void keyReleased(KeyEvent e) {}
 
 }
 
